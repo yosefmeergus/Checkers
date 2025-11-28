4fb2abe7f9c41c82ccbed816f0e8942945267856
yosefmeergus <118117060+yosefmeergus@users.noreply.github.com>
2025-11-28 18:53:10 +0200

    Feature/menu (#1)
    * player display names
    
    * created full menu workflow

--- Assets/Scripts/Managers/CheckersNetworkManager.cs
+++ Assets/Scripts/Managers/CheckersNetworkManager.cs
@@ -11,11 +11,22 @@ public class CheckersNetworkManager : NetworkManager
     [SerializeField] GameObject gameOverHandlerPrefab, boardPrefab, 
         turnsHandlerPrefab;
     public static event Action OnClientConnected;
+    [SerializeField]
+    private List<PlayerNetwork> networkPlayers = new List<PlayerNetwork>();
+
+    public List<PlayerNetwork> NetworkPlayers { get => networkPlayers; set => networkPlayers = value; }
 
     public override void OnClientConnect()
     {
         base.OnClientConnect();
         OnClientConnected?.Invoke();
+    }
+
+    public override void OnClientDisconnect()
+    {
+        base.OnClientDisconnect();
+        SceneManager.LoadScene("Lobby Scene");
+        Destroy(gameObject);
     }
 
     public override void OnServerAddPlayer(NetworkConnection connection)
@@ -24,6 +35,23 @@ public class CheckersNetworkManager : NetworkManager
         NetworkServer.AddPlayerForConnection(connection, playerInstance);   
         PlayerNetwork player = playerInstance.GetComponent<PlayerNetwork>();
         player.IsWhite = numPlayers == 1;
+        networkPlayers.Add(player);
         player.DisplayName = player.IsWhite ? "White" : "black";
+    }
+    public override void OnServerDisconnect(NetworkConnection connection)
+    {
+        base.OnServerDisconnect(connection);
+        if(connection.identity == null)
+        {
+            return;
+        }
+        PlayerNetwork player = connection.identity.GetComponent<PlayerNetwork>();
+        networkPlayers.Remove(player);
+
+    }
+
+    public override void OnStopServer()
+    {
+        networkPlayers.Clear();
     }
 }
--- Assets/Scripts/Player/PlayerNetwork.cs
+++ Assets/Scripts/Player/PlayerNetwork.cs
@@ -7,13 +7,39 @@ using UnityEngine;
 
 public class PlayerNetwork : Player
 {
-    [SyncVar]
-    private string displayName;
+    public static event Action OnPlayerInfoUpdated;
+    [SyncVar(hook = nameof(HandleDisplayNameUpdate))]
+    private string displayName = "Waiting for player";
 
     public string DisplayName
     {
         get => displayName;
         [ServerCallback]
         set => displayName = value; 
+    }
+
+    private void HandleDisplayNameUpdate(string oldValue, string newValue)
+    {
+        print("display name synced");
+        OnPlayerInfoUpdated?.Invoke();
+    }
+
+    public override void OnStartClient()
+    {
+        if(!isClientOnly)
+        {
+            return;     
+        }
+        ((CheckersNetworkManager)(NetworkManager.singleton)).NetworkPlayers.Add(this);
+    }
+
+    public override void OnStopClient()
+    {
+        if (isClientOnly)
+        {
+            return;
+        }
+        ((CheckersNetworkManager)(NetworkManager.singleton)).NetworkPlayers.Remove(this);
+        OnPlayerInfoUpdated?.Invoke();
     }
 }
