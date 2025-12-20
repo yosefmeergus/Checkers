8cd09458bf3f301f16322bd9718f680fc74de70b
yosefmeergus <118117060+yosefmeergus@users.noreply.github.com>
2025-12-20 18:55:30 +0200

    completed spawn pieces (#3)

--- Assets/Scripts/Player/PlayerPiecesHandlerNetworked.cs
+++ Assets/Scripts/Player/PlayerPiecesHandlerNetworked.cs
@@ -6,5 +6,20 @@ using UnityEngine;
 
 public class PlayerPiecesHandlerNetworked : PlayerPiecesHandler
 {
-    
+    public override void OnStartServer()
+    {
+        CheckersNetworkManager.OnGameStarted += HandleGameStarted;
+    }
+
+    public override void OnStopServer()
+    {
+        CheckersNetworkManager.OnGameStarted -= HandleGameStarted;
+    }
+
+    protected override void Spawn(GameObject prefab, int xPos, int zPos)
+    {
+        base.Spawn(prefab, xPos, zPos);
+        NetworkServer.Spawn(instanceToSpawn, connectionToClient);
+
+    }
 }
